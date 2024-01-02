fcc46a9f878339c0593cd6816a9500fe34fb158f
olayyaMansour <mansour.olayya@gmail.com>
2024-01-02 14:33:39 -0800

    fix bug

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 
 for char in range(ord('a'), ord('z') + 1):
-    if chr(char) !='e' and chr(char) != 'q':
-    print("{:c}".format(char), end='')
+    print('{:c}'.format(char), end='')
