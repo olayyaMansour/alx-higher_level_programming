0b07b8d57029e2c824d13563319bc68d193a3180
olayyaMansour <mansour.olayya@gmail.com>
2024-01-08 10:13:18 -0800

    fix bug

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if not matrix:
+        print()
+    else:
     for row in matrix:
         for elem in row:
             print("{:d}".format(elem), end=" ")
