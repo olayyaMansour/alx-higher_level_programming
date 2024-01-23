d347692a5b793ace8b42f51b8b97dd459d65d1fd
olayyaMansour <mansour.olayya@gmail.com>
2024-01-23 11:24:09 -0800

    test

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""
+Define a class Square.
+"""
+
+
+class Square:
+    """
+    Represent a square.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square (default is 0).
+        """
+        self.__size = size
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
