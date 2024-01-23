5a669ef87260f173638f29029944885dda842013
olayyaMansour <mansour.olayya@gmail.com>
2024-01-23 10:57:43 -0800

    add task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+class Square:
+    """
+    This is the Square class.
+
+    Attributes:
+        __size (int): The size of the square.
+    """
+    def __init__(self, size):
+        self.__size = size
+
