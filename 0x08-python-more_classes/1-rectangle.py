6f8d9de26a07c39c3369943e8f8372e58871587b
olayyaMansour <mansour.olayya@gmail.com>
2024-01-29 14:36:42 -0800

    fix bug

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -4,7 +4,7 @@
 
 class Rectangle:
     """Represent a rectangle."""
- 
+
     def __init__(self, width=0, height=0):
         """Initialize a rectangle with optional width and height."""
         self.width = width
