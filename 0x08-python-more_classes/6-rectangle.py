9842912b0eee6853e69b25d887043405ee79cb73
olayyaMansour <mansour.olayya@gmail.com>
2024-01-29 14:58:03 -0800

    add task 6

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -5,10 +5,13 @@
 class Rectangle:
     """Represent a rectangle."""
 
+    number_of_instances = 0  # Public class attribute
+
     def __init__(self, width=0, height=0):
         """Initialize a rectangle with optional width and height."""
         self.width = width
         self.height = height
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
@@ -63,3 +66,4 @@ class Rectangle:
     def __del__(self):
         """Print the message when an instance of Rectangle is deleted."""
         print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
