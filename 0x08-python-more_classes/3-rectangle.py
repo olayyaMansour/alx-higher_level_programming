3fddab4306b281f11bf078520ad95fc900af293a
olayyaMansour <mansour.olayya@gmail.com>
2024-01-29 14:43:18 -0800

    add task

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -4,7 +4,7 @@
 
 class Rectangle:
     """Represent a rectangle."""
-    
+
     def __init__(self, width=0, height=0):
         """Initialize a rectangle with optional width and height."""
         self.width = width
@@ -59,4 +59,3 @@ class Rectangle:
     def __repr__(self):
         """Return a representation of the rectangle."""
         return "Rectangle({}, {})".format(self.__width, self.__height)
-
