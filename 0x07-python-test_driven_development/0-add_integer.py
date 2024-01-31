7f0e366f1c7f44cabe9dc9bbaf1474e0ad9ae0c4
olayyaMansour <mansour.olayya@gmail.com>
2024-01-31 09:02:19 -0800

    update branch

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -16,6 +16,26 @@ def add_integer(a, b=98):
 
     Raises:
     TypeError: If a or b is not an integer or float.
+
+    Examples:
+    >>> add_integer(1, 2)
+    3
+    >>> add_integer(100, -2)
+    98
+    >>> add_integer(2)
+    100
+    >>> add_integer(100.3, -2)
+    98
+    >>> try:
+    ...     add_integer(4, "School")
+    ... except Exception as e:
+    ...     str(e)
+    'b must be an integer'
+    >>> try:
+    ...     add_integer(None)
+    ... except Exception as e:
+    ...     str(e)
+    'a must be an integer'
     """
 
     # Check if a is not an int or float
@@ -28,3 +48,20 @@ def add_integer(a, b=98):
 
     # Cast a and b to integers and perform the addition
     return int(a) + int(b)
+
+if __name__ == "__main__":
+    # Example usage
+    print(add_integer(1, 2))
+    print(add_integer(100, -2))
+    print(add_integer(2))
+    print(add_integer(100.3, -2))
+
+    try:
+        print(add_integer(4, "School"))
+    except Exception as e:
+        print(e)
+
+    try:
+        print(add_integer(None))
+    except Exception as e:
+        print(e)
