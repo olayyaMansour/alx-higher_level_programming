6f9be0e72935402ef024a2946cb62f02b5726e07
olayyaMansour <mansour.olayya@gmail.com>
2024-02-02 15:22:37 -0800

    test

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,21 +1,26 @@
 #!/usr/bin/python3
-"""
-This module contains a function for printing a person's name.
-"""
+
+"""Defines a function for printing a person's name."""
 
 
-def say_my_name(first_name, last_name=""):
+def print_person_name(first_name, last_name=""):
     """
     Print a formatted string with the person's name.
 
-    :param first_name: The first name as a string.
-    :param last_name: The last name as a string (default is an empty string).
-    :return: None
-    """
-    if not isinstance(first_name, str) or not isinstance(last_name, str):
-        raise TypeError("first_name must be a string or last_name must be a string")
+    Args:
+        first_name (str): The first name of the person.
+        last_name (str): The last name of the person (default is an empty string).
 
-    if last_name:
-        print("My name is {} {}".format(first_name, last_name))
-    else:
-        print("My name is {}".format(first_name))
+    Raises:
+        TypeError: If either first_name or last_name is not a string.
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+
+    print("My name is {} {}".format(first_name, last_name))
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
