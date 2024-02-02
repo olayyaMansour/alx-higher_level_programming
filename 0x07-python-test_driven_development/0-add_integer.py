7188a4145c4112b332665c2e94dbb17dd2486baa
olayyaMansour <mansour.olayya@gmail.com>
2024-02-02 12:16:07 -0800

    test

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -18,6 +18,6 @@ def add_integer(a, b=98):
 
     return int(a) + int(b)
 
-if __name__ "" "__main__":
+if __name__ == "__main__":
     import doctest
-    doctest.testfile("tests/0-add_integer.txt)"
+    doctest.testfile("tests/0-add_integer.txt")
