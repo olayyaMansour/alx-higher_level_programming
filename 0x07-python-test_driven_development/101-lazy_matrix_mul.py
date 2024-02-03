6e25327b583b3e8202c9cfe5631354bdd1eccc85
olayyaMansour <mansour.olayya@gmail.com>
2024-02-02 17:42:02 -0800

    test

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -7,9 +7,8 @@ import numpy as np
 def lazy_matrix_mul(matrix_a, matrix_b):
     """Return a multiplication of two matrices.
     Args:
-        matrix_a (list of lists of ints/floats): The first matrix.
+        matrix_g (list of lists of ints/floats): The first matrix.
         matrix_b (list of lists of ints/floats): The second matrix.
     """
 
     return np.matmul(matrix_a, matrix_b)
-
