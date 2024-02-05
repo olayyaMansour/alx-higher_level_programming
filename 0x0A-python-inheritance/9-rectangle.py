1d94908b9b9d5f296a3b07af2bfc3218e6b4e63c
olayyaMansour <mansour.olayya@gmail.com>
2024-02-05 11:49:53 -0800

    fix bug

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""Defines a Rectangle class that inherits from BaseGeometry."""
+"""Defines a class Rectangle that inherits from BaseGeometry."""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
 
 class Rectangle(BaseGeometry):
     """Represents a rectangle using the BaseGeometry class."""
@@ -12,11 +13,9 @@ class Rectangle(BaseGeometry):
             width (int): The width of the new Rectangle.
             height (int): The height of the new Rectangle.
         """
-        # Validate and set the width using the integer_validator method from the parent class
         super().integer_validator("width", width)
         self.__width = width
-        
-        # Validate and set the height using the integer_validator method from the parent class
+
         super().integer_validator("height", height)
         self.__height = height
 
@@ -25,7 +24,8 @@ class Rectangle(BaseGeometry):
         return self.__width * self.__height
 
     def __str__(self):
-        """Return a string representation of the Rectangle for print() and str()."""
-        # Create a formatted string representation with class name, width, and height
-        string_representation = f"[{self.__class__.__name__}] {self.__width}/{self.__height}"
+        """Return a string representation"""
+        string_representation = (
+                f"[{self.__class__.__name__}] {self.__width}/{self.__height}"
+                )
         return string_representation
