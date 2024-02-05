2726a8200cf13d7dc247ad829a096716a70fcb9c
olayyaMansour <mansour.olayya@gmail.com>
2024-02-05 11:26:02 -0800

    add tasks

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module for inherits_from function."""
+
+
+def inherits_from(obj, a_class):
+    """
+    Returns True if the object is an instance of a class that inherited
+    (directly or indirectly) from the specified class; otherwise, False.
+
+    Args:
+        obj: The object to check.
+        a_class: The class to compare with.
+
+    Returns:
+        True if obj is an instance of a subclass of a_class, otherwise False.
+    """
+    return issubclass(type(obj), a_class)
