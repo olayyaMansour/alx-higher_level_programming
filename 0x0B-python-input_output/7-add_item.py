958d433f9f9d967991ddaaf4585fe1cb752af12d
olayyaMansour <mansour.olayya@gmail.com>
2024-02-06 10:09:29 -0800

    test

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,22 +1,30 @@
 #!/usr/bin/python3
 """
-This script adds all arguments to a Python list, and then save them to a file.
+Script to add command line arguments to a Python list and save them to a file.
 """
 
 
 import sys
-from os.path import exists
-from json import dump, load
 
-filename = "add_item.json"
+if __name__ == "__main__":
+    # Import necessary functions from previous tasks
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_module = __import__('6-load_from_json_file')
+    load_from_json_file = load_module.load_from_json_file
 
-if exists(filename):
-    with open(filename, mode='r', encoding='utf-8') as file:
-        my_list = load(file)
-else:
-    my_list = []
+    try:
+        # Try to load items from the "add_item.json" file
+        existing_items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        # If the file doesn't exist, initialize an empty list
+        existing_items = []
 
-my_list.extend(sys.argv[1:])
+    # Extend the list with command line arguments (excluding the script name)
+    new_items = sys.argv[1:]
+    updated_items = existing_items + new_items
 
-with open(filename, mode='w', encoding='utf-8') as file:
-    dump(my_list, file)
+    # Save the updated list back to the "add_item.json" file
+    save_to_json_file(
+        updated_items,
+        "add_item.json"
+    )
