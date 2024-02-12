6d007743f38732c1ae52aa3ba173cfa32f4faf37
olayyaMansour <mansour.olayya@gmail.com>
2024-02-12 11:53:33 -0800

    add task 1

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""Defines a base model class."""
+
+
+class Base:
+    """Base class for managing id attribute."""
+
+    # Private class attribute
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Constructor for Base class.
+
+        Args:
+            id (int): Unique identifier. If not provided, it will be automatically generated.
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
