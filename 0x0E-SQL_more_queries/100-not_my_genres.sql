84c042f1d60284afda31c7261d030bcd76d00017
olayyaMansour <mansour.olayya@gmail.com>
2024-03-13 22:58:02 -0700

    add tasks

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,10 +1,11 @@
--- 14. My genres
--- Script to list all genres of the show Dexter from the database hbtn_0d_tvshows
-
+-- File: 100-not_my_genres.sql
 SELECT name
+FROM tv_genres
+WHERE name NOT IN
+(SELECT name
 FROM tv_genres
 LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-WHERE tv_shows.title = 'Dexter'
+WHERE tv_shows.title = 'Dexter')
 GROUP BY name
 ORDER BY name ASC;
