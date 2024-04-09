a1073f66ccd2863d38c3b3a24858b17ba768f4cb
olayyaMansour <mansour.olayya@gmail.com>
2024-04-08 20:32:54 -0700

    add all files

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
-const lines = [
-  'C is fun', 'Python is cool', 'JavaScript is Amazing'
-];
-for (line of lines) {
+for (const line of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
   console.log(line);
 }
