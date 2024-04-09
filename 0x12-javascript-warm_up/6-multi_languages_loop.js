96d19c2099802f37eb09e23fbddcbd2eeada565f
olayyaMansour <mansour.olayya@gmail.com>
2024-04-08 20:19:31 -0700

    fix bug

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const lines = [
-'C is fun', 'Python is cool', 'JavaScript is Amazing'
+  'C is fun', 'Python is cool', 'JavaScript is Amazing'
 ];
-lines.forEach((line) => {
-console.log(line);
-})
+for (line of lines) {
+  console.log(line);
+}
