774bc02f0c0aaa066cc044552142dd05c160df8c
olayyaMansour <mansour.olayya@gmail.com>
2024-04-26 07:14:25 -0700

    add tasks

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+A Python script that fetches https://alx-intranet.hbtn.io/status
+and displays the body response.
+Uses the requests package.
+"""
+
+import requests
+
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    response = requests.get(url)
+    print("Body response:")
+    print("\t- type:", type(response.text))
+    print("\t- content:", response.text)
