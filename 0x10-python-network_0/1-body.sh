c319458cf54d5d7397371ee7678d989c879fee86
olayyaMansour <mansour.olayya@gmail.com>
2024-04-26 09:17:57 -0700

    test

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-# Sends a GET request to a URL and displays the size of the response body
-curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
+# Sends a GET request to a URL and displays the body of the response (only for 200 status code)
+response=$(curl -sL -w "%{http_code}" -X GET "$1" -o /dev/null)
+status_code=$(echo "$response" | tail -n1)
+if [ "$status_code" -eq 200 ]; then
+    curl -sL "$1"
+else
+    echo "Error: Non-200 status code received: $status_code"
+fi
