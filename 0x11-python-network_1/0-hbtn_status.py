85edbd4633b7f7e57fe8195c05cefa70defb2293
olayyaMansour <mansour.olayya@gmail.com>
2024-04-26 07:11:03 -0700

    add task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,30 +5,12 @@ A Python script that fetches https://alx-intranet.hbtn.io/status
 
 import urllib.request
 
-def fetch_status(url):
-    """
-    Fetches the status from the given URL and returns response details.
-    
-    Args:
-        url (str): The URL to fetch status from.
-    
-    Returns:
-        dict: Dictionary containing response details.
-    """
-    req = urllib.request.Request(url)
-    with urllib.request.urlopen(req) as response:
-        html = response.read()
-        return {
-            "type": type(html),
-            "content": html,
-            "utf8_content": html.decode('utf-8')
-        }
-
 if __name__ == "__main__":
     url = 'https://alx-intranet.hbtn.io/status'
-    response_details = fetch_status(url)
-    
-    print("Body response:")
-    for key, value in response_details.items():
-        print(f"\t- {key}: {value}")
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type:", type(body))
+        print("\t- content:", body)
+        print("\t- utf8 content:", body.decode('utf-8'))
 
